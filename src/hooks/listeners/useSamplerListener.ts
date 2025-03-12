b54eb06b7dc0c4799ef8a6aa1b59f5775f01dd5b
andrii.oliinyk <andrii.oliinyk@avenga.com>
2025-03-12 18:59:07 +0200

    feat: add removing listeners after app closed

--- src/hooks/listeners/useSamplerListener.ts
+++ src/hooks/listeners/useSamplerListener.ts
@@ -5,23 +5,15 @@ import {useProjectContext} from '../../context';
 
 const useSamplerListener = () => {
   const {state, actions} = useProjectContext();
+
   useEffect(() => {
-    samplerEmitter.addListener(
-      SamplerEvents.VolumeUpdate,
-      ({id, value}) => {
-        console.log({id, value});
-        actions.updateSampleSettings(id, {volume: value});
+    samplerEmitter.addListener(SamplerEvents.VolumeUpdate, ({id, value}) => {
+      actions.updateSampleSettings(id, {volume: value});
+    });
 
-        //add ID to this because we are controlling all fader like one
-        //   if (id === data.id) {
-        // setVolume(value);
-      },
-      // });
-
-      // return () => {
-      //   samplerEmitter.removeAllListeners(SamplerEvents.VolumeUpdate);
-      // };\
-    );
+    return () => {
+      samplerEmitter.removeAllListeners(SamplerEvents.VolumeUpdate);
+    };
   }, []);
 };
 
